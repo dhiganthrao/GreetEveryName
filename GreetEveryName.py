3058be1200db59c8dbd4f94d3ca2f54fbc6f42ae
dhiganthrao <dhiganthrao@yahoo.com>
2020-03-29 20:47:09 +0530

    Changed Tweet Interval

--- GreetEveryName.py
+++ GreetEveryName.py
@@ -16,7 +16,7 @@ name = f.readline()
 while name:
     api.update_status("Hello {}! Nice to meet you.".format(name.strip()))
     name=f.readline()
-    time.sleep(600)
+    time.sleep(3600)
 f.close()
 
 
